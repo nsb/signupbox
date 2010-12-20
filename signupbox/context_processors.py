9be595fe6d435ca56232f5894bd6ae7dde977373
Niels Sandholt Busch <niels.busch@gmail.com>
2010-12-21 00:20:37 +0100

    formatting

--- signupbox/context_processors.py
+++ signupbox/context_processors.py
@@ -2,4 +2,6 @@ def account(request):
     """
     Returns the current account
     """
-    return {'account': request.user.accounts.get() if request.user.is_authenticated else None}
+    return {
+        'account': request.user.accounts.get() if request.user.is_authenticated else None
+    }
