73dc0084de55334039a2428e2bbbf70367b8a2f7
Niels Sandholt Busch <niels.busch@gmail.com>
2010-12-21 00:47:00 +0100

    fix context processor

--- signupbox/context_processors.py
+++ signupbox/context_processors.py
@@ -3,5 +3,5 @@ def account(request):
     Returns the current account
     """
     return {
-        'account': request.user.accounts.get() if request.user.is_authenticated else None
+        'account': request.user.accounts.get() if request.user.is_authenticated() else None
     }
