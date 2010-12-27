9e170b41e4fe65d75e5aca63482f33da56066c32
Niels Sandholt Busch <niels.busch@gmail.com>
2010-12-27 01:34:48 +0100

    more work on attached modal

--- media/js/modal.js
+++ media/js/modal.js
@@ -1,9 +1,18 @@
 Signupbox = {};
 (function($, $S) {
 
+  function onCloseClicked(e) {
+    $('#attached-modal-overlay, #attached-modal-container').remove();
+    e.preventDefault();
+  }
+
   function showAttachedModal(target, content) {
     $('<div id="attached-modal-overlay"></div>').appendTo(target);
-    $('<div id="attached-modal-container"></div>').append(content).appendTo(target);
+    $('<div id="attached-modal-container"></div>').
+      append(content).
+        css("top", $(target).outerHeight()).
+          appendTo(target).
+            find('.close').click(onCloseClicked);
   }
 
   $S.Modal = {
