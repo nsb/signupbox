a6d8188e75142a98ff9499195467104b21c2fd04
Niels Sandholt Busch <niels.busch@gmail.com>
2010-12-27 15:19:21 +0100

    more work on export modal

--- media/js/modal.js
+++ media/js/modal.js
@@ -10,8 +10,8 @@ Signupbox = {};
   function showAttachedModal(target, content) {
     var overlay = $('<div id="attached-modal-overlay"></div>').hide().appendTo(target);
     var container = $('<div id="attached-modal-container"></div>').hide().
-      append('<a class="button close" href="#">close</a><div class="content"></div>').
-          css("top", $(target).outerHeight()).
+      append('<div class="carat"></div><div class="content"></div>').
+          css("top", $(target).outerHeight() - 5).
             click(function(e) { e.stopPropagation(); }).
               find('.content').append(content).end().
                 find('.close').click(onCloseClicked).end().
