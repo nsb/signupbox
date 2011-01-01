f72988f049628cd13e198335b0fca69b88367e30
Niels Sandholt Busch <niels.busch@gmail.com>
2011-01-01 03:43:56 +0100

    field options

--- signupbox/forms/fields.py
+++ signupbox/forms/fields.py
@@ -1,13 +1,46 @@
 from django import forms
 from django.forms.models import modelformset_factory
 
-from ..models import Field
+from ..models import Field, FieldOption
 
-class FieldsForm(forms.ModelForm):
-    ordering = forms.IntegerField(required=False, widget=forms.HiddenInput)
+def fieldformset_factory(event):
 
-    class Meta:
-        model = Field
-        fields = ('label', 'type', 'required', 'in_extra', 'ordering',)
+    class FieldsForm(forms.ModelForm):
+        ordering = forms.IntegerField(required=False, widget=forms.HiddenInput)
 
-FieldFormset = modelformset_factory(Field, FieldsForm, extra=0, can_delete=True)
+        def save(self, *args, **kwargs):
+            self.instance.event = event
+            super(FieldsForm, self).save(*args, **kwargs)
+            self.instance.options.all().delete()
+            for value in self.data.getlist('%s-options' % self.prefix):
+                if value:
+                    FieldOption.objects.create(field=self.instance, value=value)
+            return self.instance
+
+        def has_changed(self):
+            return True
+
+        class Meta:
+            model = Field
+            fields = ('label', 'type', 'required', 'in_extra', 'ordering',)
+
+    FieldFormset = modelformset_factory(Field, FieldsForm, extra=0, can_delete=True)
+    return FieldFormset
+
+#def fieldeditform_factory(options, event=None):
+    #class FieldEditForm(forms.ModelForm):
+        #class Meta:
+            #model = Field
+            #fields = ('label', 'type', 'required', 'in_extra',)
+
+        #def save(self, *args, **kwargs):
+            #if event:
+                #self.instance.event = event
+            #super(FieldEditForm, self).save(*args, **kwargs)
+            #if 'commit' not in kwargs or kwargs['commit']:
+                #self.instance.options.all().delete()
+                #for value in options:
+                    #if value:
+                        #FieldOption.objects.create(field=self.instance, value=value)
+            #return self.instance
+    #return FieldEditForm
--- signupbox/views/fields.py
+++ signupbox/views/fields.py
@@ -6,25 +6,26 @@ from django.utils.translation import ugettext, ugettext_lazy as _
 from django.core.urlresolvers import reverse
 
 from ..models import Event
-from ..forms import FieldFormset
+from ..forms import fieldformset_factory
 
 @login_required
 def event_fields(request, slug):
 
     account=request.user.accounts.get()
     event = get_object_or_404(Event, account=account, slug=slug)
+    formset_class = fieldformset_factory(event)
 
     if request.method == 'POST':
-        formset = FieldFormset(request.POST, queryset=event.fields.all())
+        formset = formset_class(request.POST, queryset=event.fields.all())
         if formset.is_valid():
-            fields = formset.save(commit=False)
+            fields = formset.save()
             for field in fields:
                 field.event = event
                 field.save()
             messages.success(request, _('Form fields updated.'))
             return redirect(reverse('event_detail', kwargs={'slug':slug}))
     else:
-        formset = FieldFormset(queryset=event.fields.all())
+        formset = formset_class(queryset=event.fields.all())
 
     return render_to_response(
         'signupbox/event_fields.html',
