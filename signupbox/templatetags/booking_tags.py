d71bb369dff330a3d32051ab1459f06337f290f9
Niels Sandholt Busch <niels.busch@gmail.com>
2011-01-03 01:42:39 +0100

    show price on event site

--- signupbox/templatetags/booking_tags.py
+++ signupbox/templatetags/booking_tags.py
@@ -1,8 +1,20 @@
 from django import template
 from django.utils.translation import ugettext_lazy as _
+from django.db.models import Min, Max
+from django.template import defaultfilters
 
 register = template.Library()
 
 @register.simple_tag
 def ticket_summary_price(ticket, attendees):
     return ticket.price * len(attendees) or _('Free')
+
+@register.simple_tag
+def price_range(tickets):
+    min = tickets.aggregate(Min('price'))['price__min']
+    max = tickets.aggregate(Max('price'))['price__max']
+
+    if min == max:
+        return defaultfilters.floatformat(min)
+    else:
+        return '%d - %d' % (defaultfilters.floatformat(min), defaultfilters.floatformat(max))
