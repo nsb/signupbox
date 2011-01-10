b0e8bd62788fb6dfa207d2fa4af7055602df0673
Niels Sandholt Busch <niels.busch@gmail.com>
2011-01-11 00:16:22 +0100

    add fabfile

---
+++ fabfile.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+from __future__ import with_statement
+import os
+from fabric.api import *
+
+def staging():
+    """
+    staging env
+    """
+    env.hosts = ['niels@signupbox.com']
+    env.directory = '~/webapps/signupbox3/'
+    env.activate = 'workon signupbox3'
+    env.settings = 'settings_signupbox_com'
+
+## set staging as default
+staging()
+
+def test():
+    """
+    run tests before deploy
+    """
+    local('python manage.py test signupbox', capture=False)
+
+def update():
+    """
+    update src from svn
+    """
+    with cd(env.directory + 'src'):
+        run('hg pull && hg update')
+
+def reload():
+    """
+    make the webserver reload the code
+    """
+    with cd(env.directory):
+        run('apache2/bin/restart')
+
+def install_requirements():
+    """
+    install requirements
+    """
+    with cd(env.directory + 'src'):
+        virtualenv('pip install -q -r requirements.txt')
+
+def migrate():
+    """
+    migrate the database
+    """
+    with cd(env.directory + 'src'):
+        virtualenv('python manage.py syncdb --noinput --settings=' + env.settings)
+        #virtualenv('python manage.py migrate --settings=' + env.settings)
+
+def deploy():
+    """
+    deploy
+    """
+    test()
+    update()
+    install_requirements()
+    migrate()
+    reload()
+
+def virtualenv(command):
+    """
+    run command in a virtual env
+    """
+    run(env.activate + ' && ' + command)
