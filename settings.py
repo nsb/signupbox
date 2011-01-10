e53abba5ee39ce21e9dbe4c78dd0d853f8576b22
Niels Sandholt Busch <niels.busch@gmail.com>
2011-01-10 22:01:47 +0100

    add template settings

--- settings.py
+++ settings.py
@@ -87,12 +87,12 @@ LOGIN_URL = '/login/'
 
 LOGIN_REDIRECT_URL = '/'
 
-#TEMPLATE_DIRS = (
-    ## Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    ## Always use forward slashes, even on Windows.
-    ## Don't forget to use absolute paths, not relative paths.
-    #os.path.join(PROJECT_ROOT, 'templates'),
-#)
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'templates'),
+)
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
