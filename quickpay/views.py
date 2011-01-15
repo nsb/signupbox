42d7d451ed2af9810aac62ab6271c5ced7e5906a
Niels Sandholt Busch <niels.busch@gmail.com>
2011-01-16 00:47:34 +0100

    more work on payments

--- quickpay/views.py
+++ quickpay/views.py
@@ -1,5 +1,12 @@
+
 from django.views.decorators.http import require_http_methods
+from django.http import HttpResponse
+
+from forms import QuickpayForm
 
 @require_http_methods(["POST"])
 def callback(request):
-    pass
+    form = QuickpayForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return HttpResponse()
