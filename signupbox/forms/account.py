99dba040eed9f4ebebc7ff1585c882e77f2e70a7
Niels Sandholt Busch <niels.busch@gmail.com>
2011-01-15 23:37:36 +0100

    add merchant id and secret key to settings form

--- signupbox/forms/account.py
+++ signupbox/forms/account.py
@@ -6,7 +6,19 @@ class AccountForm(forms.ModelForm):
 
     class Meta:
         model = Account
-        fields = ('organization', 'street', 'zip_code', 'city', 'country', 'phone', 'cvr', 'payment_gateway', 'paypal_business',)
+        fields = (
+            'organization',
+            'street',
+            'zip_code',
+            'city',
+            'country',
+            'phone',
+            'cvr',
+            'payment_gateway',
+            'paypal_business',
+            'merchant_id',
+            'secret_key',
+        )
 
 class ProfileForm(forms.ModelForm):
 
