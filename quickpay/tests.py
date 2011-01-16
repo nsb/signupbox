1b0a188403d305ed5e305ccee455da270c2aa9d2
Niels Sandholt Busch <niels.busch@gmail.com>
2011-01-16 18:03:29 +0100

    remove test from quickpay

--- quickpay/tests.py
+++ quickpay/tests.py
@@ -11,19 +11,4 @@ class QuickpayTestCase(test.TestCase):
         pass
 
     def testQuickpay(self):
-
-        #Create an event
-        response = self.client.post(
-            reverse('event_create'), 
-            {
-                'title':'mynewtitle',
-                'begins_0':date.today() + timedelta(days=7),
-                'begins_1_0':'9',
-                'begins_1_1':'00',
-                'ends_0': date.today() + timedelta(days=7),
-                'ends_1_0':'16',
-                'ends_1_1':'00',
-            },
-        )
-        self.assertRedirects(response, reverse('event_detail', kwargs={'slug':'mynewtitle',}),)
-        self.assertTrue(Event.objects.filter(title='mynewtitle').exists())
+        pass
