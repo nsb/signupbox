005402237a6e2441c74b14e0516c2dd9520436ba
Niels Sandholt Busch <niels.busch@gmail.com>
2011-02-05 00:41:55 +0100

    more work on members

---
+++ media/js/access_permissions.js
@@ -0,0 +1,18 @@
+(function($, $S) {
+
+  function onEditPermissionsClicked(e) {
+    $(e.target).parents('.member').find('.remove').hide().end().find('.permissions').toggle(100);
+    e.preventDefault();
+  }
+
+  function onRemoveFromAccountClicked(e) {
+    $(e.target).parents('.member').find('.permissions').hide().end().find('.remove').toggle(100);
+    e.preventDefault();
+  }
+
+  $(function() {
+    $('#members .edit_permissions, #members .permissions_cancel').click(onEditPermissionsClicked)
+    $('#members .remove_from_account, #members .remove_cancel').click(onRemoveFromAccountClicked)
+  });
+
+})(jQuery, Signupbox);
