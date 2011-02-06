e7c4a861a8c7bb25f2c81ca73491449494ceb435
Niels Sandholt Busch <niels.busch@gmail.com>
2011-02-06 15:28:50 +0100

    more work on cancel invite

--- media/js/access_permissions.js
+++ media/js/access_permissions.js
@@ -10,9 +10,15 @@
     e.preventDefault();
   }
 
+  function onCancelInvitationClicked(e) {
+    $(e.target).parents('.member').find('.cancel').toggle(100);
+    e.preventDefault();
+  }
+
   $(function() {
     $('#members .edit_permissions, #members .permissions_cancel').click(onEditPermissionsClicked)
     $('#members .remove_from_account, #members .remove_cancel').click(onRemoveFromAccountClicked)
+    $('#members .cancel_invitation').click(onCancelInvitationClicked)
   });
 
 })(jQuery, Signupbox);
