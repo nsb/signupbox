52621b0fb7f26536bdc042ce05eba9c329ec33da
Niels Sandholt Busch <niels.busch@gmail.com>
2011-03-06 01:20:05 +0100

    only mail attendees with an email address

--- signupbox/tasks.py
+++ signupbox/tasks.py
@@ -29,7 +29,7 @@ def process_booking(booking):
             {'event': booking.event, 'booking': booking, 'attendee': attendee},
         ), 'noreply@%s' % Site.objects.get_current().domain, 
         [attendee.email])
-            for index, attendee in enumerate(booking.attendees.all())
+            for index, attendee in enumerate(booking.attendees.all()) if attendee.email
         )
     )
 
