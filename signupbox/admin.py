3062be894737d473a955a5c36dce810527f2124b
Niels Sandholt Busch <niels.busch@gmail.com>
2011-03-19 20:47:06 +0100

    add booking and attendee to admin

--- signupbox/admin.py
+++ signupbox/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from models import Account, AccountInvite, Event
+from models import Account, AccountInvite, Event, Booking, Attendee
 
 admin.site.register(Account)
 admin.site.register(AccountInvite)
 admin.site.register(Event)
+admin.site.register(Booking)
+admin.site.register(Attendee)
