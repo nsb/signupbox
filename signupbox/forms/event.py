bb8096ef1ae737e72a9d670744b293e71e4d34bf
Niels Sandholt Busch <niels.busch@gmail.com>
2011-03-23 22:01:02 +0100

    translations

--- signupbox/forms/event.py
+++ signupbox/forms/event.py
@@ -7,8 +7,8 @@ from django.utils.translation import ugettext, ugettext_lazy as _
 class EventForm(forms.ModelForm):
     begins = DateTimeField(required=True, widget=DateTimeWidget, label=_('Begins'))
     ends = DateTimeField(required=True, widget=DateTimeWidget, label=_('Ends'))
-    description = forms.CharField(
-        widget=forms.Textarea(attrs={'class': 'mceEditor'}), required=False)
+    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'mceEditor'}),
+        label=_('Description'), required=False)
 
     class Meta:
         model = Event
