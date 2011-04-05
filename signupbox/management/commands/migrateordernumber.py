075022d6da58ce673cf8b9d15de5b79755f4b461
Niels Sandholt Busch <niels.busch@gmail.com>
2011-04-05 22:58:19 +0200

    add migrateordernumber

--- signupbox/management/commands/migrateordernumber.py
+++ signupbox/management/commands/migrateordernumber.py
@@ -1,61 +1,38 @@
-#import psycopg2
-#from psycopg2.extras import DictCursor 
+import psycopg2
+from psycopg2.extras import DictCursor 
 
-#from django.core.management.base import BaseCommand, CommandError
-#from django.db import transaction
-#from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
+from django.contrib.auth.models import User
 
-#from signupbox.constants import *
-#from signupbox.models import Account, Event, Booking, Ticket, Field, FieldValue, FieldOption, Attendee
-#from signupbox.signals import booking_confirmed
+from signupbox.constants import *
+from signupbox.models import Booking
 
-#class Command(BaseCommand):
+class Command(BaseCommand):
 
-    #def handle(self, *args, **options):
-        #conn = psycopg2.connect(
-              #"dbname = signupbox user = niels"
-        #)
+    def handle(self, *args, **options):
+        conn = psycopg2.connect(
+            "dbname = signupbox_old user = niels"
+        )
 
-        #try:
-            #transaction.enter_transaction_management()
+        try:
+            transaction.enter_transaction_management()
 
-            #account_cur = conn.cursor(cursor_factory=DictCursor)
-            #account_cur.execute("SELECT * from accounts_account;")
-            #for account in account_cur:
-                #print 'fixing account %s' % account['name']
+            booking_cur = conn.cursor(cursor_factory=DictCursor)
+            booking_cur.execute("SELECT * from core_booking;")
+            for booking in booking_cur:
+                try:
+                    b = Booking.objects.get(
+                        transaction=booking['transaction'],
+                        ordernumber=1
+                    )
+                    b.ordernumber = booking['ordernum']
+                    b.save()
+                    print "updated booking %s with ordernumber %s" % (b, booking['ordernum'])
+                except (Booking.DoesNotExist, Booking.MultipleObjectsReturned):
+                    continue
 
-                ## create account
-                #a, created = Account.objects.get_or_create(
-                    #name=account['name']
-                #)
-
-                #event_cur = conn.cursor(cursor_factory=DictCursor)
-                #event_cur.execute("SELECT * from core_event where account_id = %s;", (account['id'],))
-
-                #for event in event_cur:
-                    #print '\tfixing event %s' % event['title']
-
-                    #e, created = Event.objects.get_or_create(
-                        #account = a,
-                        #title = event['title'],
-                        #begins = event['start'],
-                        #ends = event['end'],
-                    #)
-
-                    ## add bookings
-                    #booking_cur = conn.cursor(cursor_factory=DictCursor)
-                    #booking_cur.execute("SELECT * from core_booking where event_id = %s;", (event['id'],))
-                    #for booking in booking_cur:
-                        #try:
-                            #b = Booking.objects.get(
-                                #timestamp = booking['timestamp']
-                            #)
-                            #print b
-                        #except Booking.DoesNotExist:
-                            #print 'ouch'
-                            #continue
-
-            #transaction.commit()
-        #finally:
-            #transaction.leave_transaction_management()
-        #print 'Successfully migrated signupbox!'
+            transaction.commit()
+        finally:
+            transaction.leave_transaction_management()
+        print 'Successfully migrated signupbox!'
