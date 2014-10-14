9f0e622f80f8cfe6c4ed97f069238e552e19a4b6
Niels Sandholt Busch <niels.busch@gmail.com>
2014-10-14 16:17:55 +0200

    fix settings

--- settings.py
+++ settings.py
@@ -205,7 +205,7 @@ CELERYD_CONCURRENCY = 1
 CELERYBEAT_SCHEDULE = {
     'run-survey-every-hour': {
         'task': 'signupbox.tasks.run_surveys',
-        'schedule': timedelta(minutes=1),
+        'schedule': timedelta(hours=1),
     },
     'run-send-reminders-every-hour': {
         'task': 'signupbox.tasks.send_reminders',
